85637e36d31a404cd6c633c8215dfc379a08e486
Martin Billinger <martin.billinger@medel.com>
2019-02-15 12:20:03 +0100

    basic concepts

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,159 @@
+use std::cell::{RefCell, RefMut, Ref};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::rc::Rc;
-use std::collections::{HashMap, VecDeque};
 
+
+#[derive(Clone)]
+enum Callable {
+    Native(fn(&mut State)),
+
+}
+
+impl Callable {
+    fn invoke(&self, state: &mut State) {
+        match self {
+            Callable::Native(fun) => fun(state),
+        }
+    }
+}
+
+
+#[derive(Clone)]
 enum Object {
     None,
-    Native(fn(&mut State)),
+    Callable(Callable),
+    List(Rc<RefCell<Vec<Object>>>),
+    String(Rc<String>),
+    I32(i32),
+}
+
+impl std::fmt::Debug for Object {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Object::None => write!(f, "None"),
+            Object::Callable(_) => write!(f, "<func>"),
+            Object::List(list) => write!(f, "{:?}", list),
+            Object::String(rcs) => write!(f, "{:?}", rcs),
+            Object::I32(i) => write!(f, "{:?}", i),
+        }
+    }
+}
+
+impl From<Rc<String>> for Object {
+    fn from(s: Rc<String>) -> Object {
+        Object::String(s)
+    }
+}
+
+impl From<i32> for Object {
+    fn from(i: i32) -> Object {
+        Object::I32(i)
+    }
+}
+
+impl Object {
+    fn invoke(self, state: &mut State) {
+        match self {
+            Object::Callable(fun) => fun.invoke(state),
+            other => state.push(other)
+        }
+    }
+
+    fn as_vec_mut(&mut self) -> RefMut<Vec<Object>> {
+        match self {
+            Object::List(vec) => vec.borrow_mut(),
+            _ => panic!("Type Error"),
+        }
+    }
+
+    fn as_vec(&self) -> Ref<Vec<Object>> {
+        match self {
+            Object::List(vec) => vec.borrow(),
+            _ => panic!("Type Error"),
+        }
+    }
+}
+
+/// Wrap `Rc<String>` so that we can implement `Borrow<str>` on it
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct RcString(Rc<String>);
+
+impl std::borrow::Borrow<str> for RcString {
+    fn borrow(&self) -> &str {
+        &self.0[..]
+    }
 }
 
 
+/// will be responsible for things like string and small integer reuse
+#[derive(Debug)]
+struct ObjectFactory {
+    strings: HashSet<RcString>,
+}
+
+impl ObjectFactory {
+    fn new() -> Self {
+        ObjectFactory {
+            strings: HashSet::new(),
+        }
+    }
+
+    fn parse(&mut self, s: &str) -> Option<Object> {
+        if s.starts_with('"') && s.ends_with('"') {
+            Some(self.get_string(&s[1..s.len()-1]).into())
+        } else {
+            s.parse::<i32>().ok().map(Object::from)
+        }
+    }
+
+    fn get_string(&mut self, s: &str) -> Rc<String> {
+        if let Some(rcs) = self.strings.get(s) {
+            rcs.0.clone().into()
+        } else {
+            let rcs = RcString(Rc::new(s.to_owned()));
+            self.strings.insert(rcs.clone());
+            rcs.0.into()
+        }
+    }
+
+    fn new_list(&self) -> Object {
+        Object::List(Rc::new(RefCell::new(vec![])))
+    }
+}
+
+#[derive(Debug)]
 enum Entry {
-    Object(Rc<Object>)
+    Word(Object),
+    ParsingWord(Object),
 }
 
-
+#[derive(Debug)]
 struct Dictionary {
-    words: HashMap<String, Entry>
+    words: HashMap<RcString, Entry>,
 }
 
 impl Dictionary {
     fn new() -> Self {
         Dictionary {
-            words: HashMap::new()
+            words: HashMap::new(),
         }
     }
 
-    fn insert(&mut self, key: String, val: Entry) {
-        self.words.insert(key, val);
+    fn insert(&mut self, key: Rc<String>, val: Entry) {
+        self.words.insert(RcString(key), val);
+    }
+
+    fn lookup(&self, key: &str) -> Option<&Entry> {
+        self.words.get(key)
     }
 }
 
-
-struct State {
+#[derive(Debug)]
+pub struct State {
     input_tokens: VecDeque<String>,
-    stack: Vec<Rc<Object>>,
+    stack: Vec<Object>,
     dictionary: Dictionary,
+    factory: ObjectFactory,
 }
 
 impl State {
@@ -41,21 +162,54 @@ impl State {
             input_tokens: VecDeque::new(),
             stack: vec![],
             dictionary: Dictionary::new(),
+            factory: ObjectFactory::new(),
         }
     }
 
     pub fn run(&mut self, input: &str) {
-        self.input_tokens.extend(tokenize(input).map(str::to_string));
+        self.input_tokens
+            .extend(tokenize(input).map(str::to_string));
+        self.push(self.factory.new_list());
         while let Some(token) = self.input_tokens.pop_front() {
-            unimplemented!()
+            let literal = self.factory.parse(&token);
+            let word = self.dictionary.lookup(&token);
+            match (literal, word) {
+                (None, None) => panic!("Unknown Word: {}", token),
+                (Some(_), Some(_)) => panic!("Ambiguous Word: {}", token),
+                (Some(obj), None) => self.top_mut().as_vec_mut().push(obj),
+                (None, Some(Entry::Word(obj))) => {
+                    let obj = obj.clone();
+                    self.top_mut().as_vec_mut().push(obj);
+                }
+                (None, Some(Entry::ParsingWord(obj))) => obj.clone().invoke(self),
+            }
+        }
+        let ops = self.pop();
+        self.run_sequence(ops.as_vec().as_slice());
+    }
+
+    fn run_sequence(&mut self, ops: &[Object]) {
+        for op in ops {
+            op.clone().invoke(self);
         }
     }
 
+    fn push(&mut self, obj: Object) {
+        self.stack.push(obj);
+    }
+
+    fn pop(&mut self) -> Object {
+        self.stack.pop().expect("Stack Empty")
+    }
+
+    fn top_mut(&mut self) -> &mut Object {
+        self.stack.last_mut().expect("Stack Empty")
+    }
+
     pub fn tier0(&mut self) {
-        unimplemented!()
+        self.dictionary.insert(self.factory.get_string(".s"), Entry::Word(Object::Callable(Callable::Native(|state| println!("{:?}", state.stack)))));
     }
 }
-
 
 fn tokenize(input: &str) -> impl Iterator<Item = &str> {
     let mut it = input.char_indices().peekable();
@@ -89,7 +243,10 @@ fn tokenize(input: &str) -> impl Iterator<Item = &str> {
         .map(Option::unwrap)
 }
 
-fn skip_while(it: &mut std::iter::Peekable<std::str::CharIndices>, predicate: impl Fn(char) -> bool) {
+fn skip_while(
+    it: &mut std::iter::Peekable<std::str::CharIndices>,
+    predicate: impl Fn(char) -> bool,
+) {
     while let Some(true) = it.peek().map(|&(_, ch)| predicate(ch)) {
         it.next();
     }
@@ -102,6 +259,13 @@ mod tests {
     #[test]
     fn it_works() {
         let mut state = State::new();
-        state.run("3 5 +");
+        state.tier0();
+
+        state.run("3 5 \"hello forth!\" .s");
+        state.run("3 5 \"hello forth!\" .s");
+
+        println!("{:#?}", state);
+
+        panic!("panicking so we can see the output :)");
     }
 }
