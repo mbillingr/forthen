52e10f824dae1c764e2c6b4acbcbd268597daef8
Martin Billinger <martin.billinger@medel.com>
2019-03-01 10:27:03 +0100

    rename duplicate effect vars

--- src/stack_effect.rs
+++ src/stack_effect.rs
@@ -1,6 +1,6 @@
 use crate::abstract_stack::{AbstractStack, Sequence, StackItem};
 use crate::parsing::tokenize;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub trait IntoStackEffect {
     fn into_stack_effect(self) -> StackEffect;
@@ -54,19 +54,18 @@ impl StackEffect {
     }
 
     pub fn chain(&self, rhs: &Self) -> Self {
+        let (a, b) = rename_effects(self, rhs);
+
         let mut astack = AbstractStack::new();
-        astack.apply_effect(self).unwrap();
-        astack.apply_effect(rhs).unwrap();
+        astack.apply_effect(&a).unwrap();
+        astack.apply_effect(&b).unwrap();
 
         loop {
             match (astack.inputs.front(), astack.outputs.front()) {
                 (None, _) | (_, None) => break,
                 (Some(a), Some(b)) if a != b => break,
                 (Some(a), Some(b)) if a == b => {
-                    if astack.outputs.values[1..]
-                        .iter()
-                        .any(|b| a == b)
-                    {
+                    if astack.outputs.values[1..].iter().any(|b| a == b) {
                         break;
                     }
                 }
@@ -82,10 +81,73 @@ impl StackEffect {
             outputs: astack.outputs.into(),
         }
     }
+
+    fn all_names(&self) -> HashSet<&str> {
+        let mut names = HashSet::new();
+        names.extend(self.inputs.iter().flat_map(|i| i.all_names()));
+        names.extend(self.outputs.iter().flat_map(|o| o.all_names()));
+        names
+    }
+
+    fn renamed(&self, mapping: &HashMap<&str, String>) -> StackEffect {
+        StackEffect {
+            inputs: self.inputs.iter().map(|i| i.renamed(mapping)).collect(),
+            outputs: self.outputs.iter().map(|o| o.renamed(mapping)).collect(),
+        }
+    }
 }
 
-fn compare_sequence<'a>(seq_a: &'a [EffectNode], seq_b: &'a [EffectNode], pos_a: &mut HashMap<&'a str, usize>, pos_b: &mut HashMap<&'a str, usize> ) -> bool {
-    for (a, b) in seq_a.iter().zip(seq_b) {            
+fn rename_effects(left: &StackEffect, right: &StackEffect) -> (StackEffect, StackEffect) {
+    let left_names = left.all_names();
+    let right_names = right.all_names();
+    let dups: HashSet<&str> = left_names.union(&right_names).cloned().collect();
+
+    let left_rename: HashMap<_, _> = left_names
+        .into_iter()
+        .map(|name| {
+            (
+                name,
+                if dups.contains(&name) {
+                    add_to_name(name, 0)
+                } else {
+                    name.to_string()
+                },
+            )
+        })
+        .collect();
+
+    let right_rename: HashMap<_, _> = right_names
+        .into_iter()
+        .map(|name| {
+            (
+                name,
+                if dups.contains(&name) {
+                    add_to_name(name, 1)
+                } else {
+                    name.to_string()
+                },
+            )
+        })
+        .collect();
+
+    (left.renamed(&left_rename), right.renamed(&right_rename))
+}
+
+fn add_to_name(name: &str, n: i32) -> String {
+    let i = name.len() - name.chars().rev().filter(|ch| ch.is_digit(10)).count();
+
+    let number: i32 = name[i..].parse().unwrap_or(0);
+
+    format!("{}{}", &name[..i], number + n)
+}
+
+fn compare_sequence<'a>(
+    seq_a: &'a [EffectNode],
+    seq_b: &'a [EffectNode],
+    pos_a: &mut HashMap<&'a str, usize>,
+    pos_b: &mut HashMap<&'a str, usize>,
+) -> bool {
+    for (a, b) in seq_a.iter().zip(seq_b) {
         let n = pos_a.len();
         let m = pos_b.len();
         let i = pos_a.entry(a.name()).or_insert(n);
@@ -98,11 +160,18 @@ fn compare_sequence<'a>(seq_a: &'a [EffectNode], seq_b: &'a [EffectNode], pos_a:
     true
 }
 
-fn compare_effects(in_a: &[EffectNode], out_a: &[EffectNode], in_b: &[EffectNode], out_b: &[EffectNode]) -> bool {
+fn compare_effects(
+    in_a: &[EffectNode],
+    out_a: &[EffectNode],
+    in_b: &[EffectNode],
+    out_b: &[EffectNode],
+) -> bool {
     let mut self_pos = HashMap::new();
     let mut other_pos = HashMap::new();
 
-    if !compare_sequence(in_a, in_b, &mut self_pos, &mut other_pos) { return false }
+    if !compare_sequence(in_a, in_b, &mut self_pos, &mut other_pos) {
+        return false;
+    }
     compare_sequence(out_a, out_b, &mut self_pos, &mut other_pos)
 }
 
@@ -135,6 +204,35 @@ impl EffectNode {
             }
         }
     }
+
+    fn all_names(&self) -> HashSet<&str> {
+        let mut names = HashSet::new();
+
+        match self {
+            EffectNode::Row(name) | EffectNode::Item(name) => {
+                names.insert(name.as_str());
+            }
+            EffectNode::Quotation(name, a, b) => {
+                names.insert(name);
+                names.extend(a.iter().flat_map(|i| i.all_names()));
+                names.extend(b.iter().flat_map(|o| o.all_names()));
+            }
+        }
+
+        names
+    }
+
+    fn renamed(&self, mapping: &HashMap<&str, String>) -> EffectNode {
+        match self {
+            EffectNode::Row(name) => EffectNode::Row(mapping[name.as_str()].clone()),
+            EffectNode::Item(name) => EffectNode::Item(mapping[name.as_str()].clone()),
+            EffectNode::Quotation(name, a, b) => EffectNode::Quotation(
+                mapping[name.as_str()].clone(),
+                a.iter().map(|i| i.renamed(mapping)).collect(),
+                b.iter().map(|o| o.renamed(mapping)).collect(),
+            ),
+        }
+    }
 }
 
 impl From<Sequence> for Vec<EffectNode> {
@@ -163,11 +261,13 @@ impl std::cmp::PartialEq for EffectNode {
             (Quotation(_, ia, oa), Quotation(_, ib, ob)) => {
                 let mut a_pos = HashMap::new();
                 let mut b_pos = HashMap::new();
-                if !compare_sequence(ia, ib, &mut a_pos, &mut b_pos) { return false }
+                if !compare_sequence(ia, ib, &mut a_pos, &mut b_pos) {
+                    return false;
+                }
                 compare_sequence(oa, ob, &mut a_pos, &mut b_pos)
             }
             _ => false,
-        }        
+        }
     }
 }
 
@@ -185,9 +285,7 @@ impl std::fmt::Debug for EffectNode {
     }
 }
 
-fn parse_effect<'a>(
-    input: &mut std::iter::Peekable<impl Iterator<Item = &'a str>>
-) -> StackEffect {
+fn parse_effect<'a>(input: &mut std::iter::Peekable<impl Iterator<Item = &'a str>>) -> StackEffect {
     assert_eq!(input.next(), Some("("));
     let inputs = parse_sequence(input, "--");
     let outputs = parse_sequence(input, ")");
