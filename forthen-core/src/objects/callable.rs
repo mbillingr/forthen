d0c52b39441fde5cf099c0bb84e39240ebebcfeb
Martin Billinger <flkazemakase@gmail.com>
2019-03-05 23:41:20 +0100

    working on interfaces

--- forthen-core/src/objects/callable.rs
+++ forthen-core/src/objects/callable.rs
@@ -1,6 +1,13 @@
 use crate::errors::Result;
 use crate::State;
+use crate::Object;
 use std::rc::Rc;
 
+/// A pure function has no side effects. It can only access the value stack.
+pub type PureFunction = fn(&mut Vec<Object>) -> Result<()>;
+
+/// A native function can manipulate the entire state.
 pub type NativeFunction = fn(&mut State) -> Result<()>;
+
+/// A closure is allowed to contain external state.
 pub type NativeClosure = Rc<dyn Fn(&mut State) -> Result<()>>;
--- forthen-core/src/objects/prelude.rs
+++ forthen-core/src/objects/prelude.rs
@@ -1,5 +1,8 @@
 pub use super::Object;
 use std::any::Any;
+use crate::State;
+use crate::Result;
+use crate::StackEffect;
 
 pub trait DynamicObject {
     fn as_any(&self) -> &dyn Any;
@@ -11,14 +14,40 @@ pub trait DynamicObject {
 
 pub trait ObjectInterface {
     fn as_any(&self) -> &dyn Any;
-
     fn as_number(&self) -> Option<&dyn NumberInterface>;
+    fn as_callable(&self) -> Option<&dyn NumberInterface>;
+    fn as_sequence(&self) -> Option<&dyn SequenceInterface>;
+
+    fn repr(&self, _state: &mut State) -> Result<()>;
+
+    fn cmp_equal(&self, _state: &mut State) -> Result<()>;
 
     fn is_number(&self) -> bool {
         self.as_number().is_some()
     }
+
+    fn is_callable(&self) -> bool {
+        self.as_callable().is_some()
+    }
+
+    fn is_sequence(&self) -> bool {
+        self.as_sequence().is_some()
+    }
 }
 
-pub trait NumberInterface {}
+pub trait NumberInterface {
+    fn add(&self, _state: &mut State) -> Result<()>;
 
-pub trait CallableInterface {}
+    fn push_integer(&self, _state: &mut State) -> Result<()>;
+    fn push_float(&self, _state: &mut State) -> Result<()>;
+}
+
+pub trait CallableInterface {
+    fn get_stack_effect(&self) -> StackEffect;
+    fn call(&self, _state: &mut State) -> StackEffect;
+}
+
+pub trait SequenceInterface {
+    fn as_vec_mut(&mut self) -> Result<&mut Vec<Object>>;
+    fn as_slice(&self) -> Result<&[Object]>;
+}
