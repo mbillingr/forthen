650125a040533cd0c835d7ca07e02f5b5239b7f5
Martin Billinger <flkazemakase@gmail.com>
2019-03-10 17:54:28 +0100

    add recursion test

--- forthen-std/src/lib.rs
+++ forthen-std/src/lib.rs
@@ -9,3 +9,20 @@ pub use complex::complex;
 pub use ops::ops;
 pub use tier0::tier0;
 pub use tier1::tier1;
+
+
+#[cfg(test)]
+mod tests {
+    use forthen_core::State;
+    use super::*;
+
+    #[test]
+    fn recursion() {
+        let mut state = State::new();
+        tier0(&mut state).unwrap();
+        tier1(&mut state).unwrap();
+        ops(&mut state).unwrap();
+        state.run(": recursive [ 1 - dup 0 == [ dup . drop drop ] [ dup . swap dup rot swap call ] if ] dup rot swap call ;").unwrap();
+        state.run("10 recursive").unwrap();
+    }
+}
